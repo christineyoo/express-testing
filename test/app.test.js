c57ed2bfd7a2ed8f80bc915f5f157ab36611ebfc
Christine Yoo <christinecho.s@gmail.com>
2021-03-23 17:32:01 -0700

    implemented testing express server

--- test/app.test.js
+++ test/app.test.js
@@ -7,3 +7,17 @@ describe("Express App", () => {
     return supertest(app).get("/").expect(200, "Hello Express!");
   });
 });
+
+
+describe('GET /quotient', () => {
+  it('8/4 should be 2', () => {
+    return supertest(app)
+      .get('/quotient')
+      .query({ a: 8, b: 4 })
+      .expect(200, '8 divided by 4 is 2');
+  });
+
+  it('should return 400 if "a" is missing', () => {
+    return supertest(app).get('/quotient').query({ b: 4 }).expect(400, 'Value for a is needed');
+  })
+});
